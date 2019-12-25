12b24553aadcf22fd12e00a1c3dfdabbfbece65d
karan-singare <karansingare@gmail.com>
2019-12-26 05:23:59 +0530

    smooth scrolling enabled

---
+++ js/smooth-scroll.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  var scrollLink = $(".scroll");
+
+  //Smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000);
+  });
+
+  //Active links switch
+
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function() {
+      var sectionOffset = $(this.hash).offset().top;
+      console.log(sectionOffset);
+
+
+      if (sectionOffset <= scrollbarLocation) {
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
+      }
+    });
+  });
+});
