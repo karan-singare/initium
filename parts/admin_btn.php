72d9dc8dcab40cb55d6484cbe3f014ae57553171
karan-singare <karansingare@gmail.com>
2020-01-04 02:18:23 +0530

    .

--- parts/admin_btn.php
+++ parts/admin_btn.php
@@ -1,11 +1,14 @@
 <?php require_once 'connection/db.php'; ?>
 <?php
-  $username = $_SESSION['username'];
-  $query = $db->prepare("select admin from users where username = ?");
-  $query->execute([$username]);
-  $result = $query->fetch(PDO::FETCH_OBJ);
+  if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+    $query = $db->prepare("select admin from users where username = ?");
+    $query->execute([$username]);
+    $result = $query->fetch(PDO::FETCH_OBJ);
 
-  if ($result->admin == "true") {
-    echo "<a href='admin' class='btn btn--pink admin-btn'>View data</a>";
+    if ($result->admin == "true") {
+      echo "<a href='admin' class='btn btn--pink admin-btn'>View data</a>";
+    }
+
   }
 ?>
